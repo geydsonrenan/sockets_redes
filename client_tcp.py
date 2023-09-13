ed640b03ba294ee93d13e2c9ea0cf2dc934ac3fe
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-13 14:48:13 -0300

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -1,10 +1,44 @@
 from socket import *
+import time
 
-servername = 'localhost'
-serverport = 55080
-clientsocket = socket(AF_INET, SOCK_STREAM)
-clientsocket.connect((servername, serverport))
+name_server = "localhost"
+dns_port = 51009
 
-while 42:
-    msg = input('digite: ')
-    clientsocket.send(msg.encode()) 
+anos  = ["06/10", "01/09", "19/03", "22/01", "29/12"]
+
+def request_dns(name_server, dns_port):
+    msg = f"request,{name_server}, UDP"
+    sock_client = socket(AF_INET, SOCK_DGRAM)
+    sock_client.bind(("localhost", 53000))
+    sock_client.sendto(msg.encode(), (name_server, dns_port))
+    msg_resp , ip = sock_client.recvfrom(1024)
+    sock_client.close()
+    print(msg_resp.decode())
+    return msg_resp.decode()
+
+server_port = request_dns(name_server, dns_port)
+#mudar isso:
+#clientsocket = socket(AF_INET, SOCK_STREAM)
+
+#clientsocket.connect(("localhost", int(server_port)))
+#clientsocket.bind(('localhost', 53000))
+messagem = "informe o ano de nascimento"
+tempo_inicial = time.perf_counter()
+
+for i in range(len(anos)):
+    clientsocket = socket(AF_INET, SOCK_STREAM)
+    clientsocket.connect(("localhost", int(server_port)))
+    msg = anos[i] + '/' + 'localhost' + '/' + '53000'
+    clientsocket.send(msg.encode())
+    clientsocket.close()
+    while 42:
+        clientsocket = socket(AF_INET, SOCK_STREAM)
+        clientsocket.bind(('localhost', 53000))
+        clientsocket.listen(1)
+        resp, address = clientsocket.accept()
+        resp = resp.recv(1024)
+        print(resp.decode())
+        clientsocket.close()
+        break
+tempo_final = time.perf_counter()
+print(f'tempo para resoluir as 5 requisições: {tempo_final - tempo_inicial}')
