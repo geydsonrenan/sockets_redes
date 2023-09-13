39a457c05d487698931dd44786078ddd80eb2f40
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2023-09-12 23:12:39 -0300

    Update dns.py

--- dns.py
+++ dns.py
@@ -2,7 +2,7 @@ from socket import *
 
 dns = {}
 
-name = "localhost"
+name = "127.0.0.1"
 port = 51009
 
 server = socket(AF_INET, SOCK_DGRAM)
@@ -13,11 +13,13 @@ print("Server ON")
 while 42:
     msg, ip = server.recvfrom(1024)
     msg = msg.decode()
-    msg = msg.split()
+    msg = msg.split(',')
     
-    if msg[0] == "start":
-        dns[msg[1]] = ip
+    if msg[0] == "gravar":
+        dns[msg[1]] = msg[2]
+        print(ip)
         print(f"registrou o {msg[1]} no DNS")
+        print(dns)
     else:
         msg[0] == "search"
         if msg[1] in dns:
