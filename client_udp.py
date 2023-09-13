786da08c1d06ab1579d388789f363ebf1b0865a9
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-13 14:48:36 -0300

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -1,8 +1,34 @@
 from socket import *
+import time
 
-name_server = 'localhost'
-server_port = 51001
-client = socket(AF_INET, SOCK_DGRAM)
-messagem = input('Digite:')
-client.sendto(messagem.encode(), (name_server, server_port))
-client.close()
+name_server = "localhost"
+dns_port = 51009
+
+anos = ["06/10", "01/09", "19/03", "22/01", "29/12"]
+
+def request_dns(name_server, dns_port):
+    msg = f"request,{name_server}, UDP"
+    sock_client = socket(AF_INET, SOCK_DGRAM)
+    sock_client.bind(("localhost", 52000))
+    sock_client.sendto(msg.encode(), (name_server, dns_port))
+    msg_resp , ip = sock_client.recvfrom(1024)
+    sock_client.close()
+    print(msg_resp.decode())
+    return msg_resp.decode()
+
+server_port = request_dns(name_server, dns_port)
+print("Server ON!")
+messagem = "informe o ano de nascimento.."
+tempo_inicial = time.perf_counter()
+
+for i in range(len(anos)):
+    client = socket(AF_INET, SOCK_DGRAM)
+    client.bind(("localhost", 52000))
+    msg = anos[i] + '/' + 'localhost' + '/' + '52000'
+    client.sendto(msg.encode(), (name_server, int(server_port)))
+    resp, ip = client.recvfrom(1024)
+    print(resp.decode())
+    client.close()
+
+tempo_final = time.perf_counter()
+print(f'tempo para resoluir as 5 requisições: {tempo_final - tempo_inicial}')
