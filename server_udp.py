03430db64ed98a77f4bd2ea221509b1711c6fba1
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-13 01:04:47 -0300

    Update server_udp.py

--- server_udp.py
+++ server_udp.py
@@ -2,8 +2,10 @@ from socket import *
 
 name_server = 'localhost'
 server_port = 51001
+
 name_dns = "localhost"
 dns_port = 51009
+
 print('SERVER ON')
 
 def conection_dns(name_serv, serv_port, dns_name, dns_port):
@@ -11,9 +13,12 @@ def conection_dns(name_serv, serv_port, dns_name, dns_port):
     sock_serv = socket(AF_INET, SOCK_DGRAM)
     sock_serv.sendto(msg.encode(), (dns_name, dns_port))
     sock_serv.close()
+    
 conection_dns(name_server, server_port, name_dns, dns_port)
 server = socket(AF_INET, SOCK_DGRAM)
 server.bind((name_server, server_port))
+
 while 42:
     sck_client, address = server.recvfrom(1024)
     print(sck_client.decode())
+    
