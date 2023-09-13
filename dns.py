34a4157725f4207067e12c10b8d4270eef86b79e
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-12 22:30:58 -0300

    Add files via upload

---
+++ dns.py
@@ -0,0 +1,26 @@
+from socket import *
+
+dns = {}
+
+name = "localhost"
+port = 51009
+
+server = socket(AF_INET, SOCK_DGRAM)
+
+server.bind((name, port))
+
+while 42:
+    msg, ip = server.recvfrom(1024)
+    msg = msg.decode()
+    msg = msg.split()
+    
+    if msg[0] == "start":
+        dns[msg[1]] = ip
+        print(f"registrou o {msg[1]} no DNS")
+    else:
+        msg[0] == "search"
+        if msg[1] in dns:
+            print(dns[msg[1]])
+        else:
+            print("DNS não encontrado")
+        
