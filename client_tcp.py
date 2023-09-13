89c950909c8df5135e6c6db51d0b0950f297a8ba
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2023-09-13 19:00:05 -0300

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -19,27 +19,18 @@ def request_dns(name_server, dns_port):
 
 server_port = request_dns(name_server, dns_port)
 #mudar isso:
-#clientsocket = socket(AF_INET, SOCK_STREAM)
+clientsocket = socket(AF_INET, SOCK_STREAM)
 
-#clientsocket.connect(("localhost", int(server_port)))
-#clientsocket.bind(('localhost', 53000))
+clientsocket.connect(("localhost", int(server_port)))
+
 messagem = "informe o ano de nascimento"
 tempo_inicial = time.perf_counter()
 
 for i in range(len(anos)):
-    clientsocket = socket(AF_INET, SOCK_STREAM)
-    clientsocket.connect(("localhost", int(server_port)))
     msg = anos[i] + '/' + 'localhost' + '/' + '53000'
     clientsocket.send(msg.encode())
-    clientsocket.close()
-    while 42:
-        clientsocket = socket(AF_INET, SOCK_STREAM)
-        clientsocket.bind(('localhost', 53000))
-        clientsocket.listen(1)
-        resp, address = clientsocket.accept()
-        resp = resp.recv(1024)
-        print(resp.decode())
-        clientsocket.close()
-        break
+    resp = clientsocket.recv(1024)
+    print(resp.decode())
+clientsocket.close()
 tempo_final = time.perf_counter()
 print(f'tempo para resoluir as 5 requisições: {tempo_final - tempo_inicial}')
