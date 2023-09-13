1a8bcecd619964210f1b1233eb10b6565eadb754
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2023-09-13 14:12:00 -0300

    Update client_tcp.py

--- client_tcp.py
+++ client_tcp.py
@@ -1,4 +1,5 @@
 from socket import *
+import time
 
 name_server = "localhost"
 dns_port = 51009
@@ -17,10 +18,27 @@ def request_dns(name_server, dns_port):
 
 server_port = request_dns(name_server, dns_port)
 #mudar isso:
-clientsocket = socket(AF_INET, SOCK_STREAM)
-clientsocket.connect(("localhost", 53000))
-messagem = "informe o ano de nascimento"
+#clientsocket = socket(AF_INET, SOCK_STREAM)
 
-while 42:
-    msg = input('digite: ')
-    clientsocket.send(msg.encode()) 
+#clientsocket.connect(("localhost", int(server_port)))
+#clientsocket.bind(('localhost', 53000))
+messagem = "informe o ano de nascimento"
+tempo_inicial = time.perf_counter()
+
+for i in range(len(anos)):
+    clientsocket = socket(AF_INET, SOCK_STREAM)
+    clientsocket.connect(("localhost", int(server_port)))
+    msg = anos[i] + '/' + 'localhost' + '/' + '53000'
+    clientsocket.send(msg.encode())
+    clientsocket.close()
+    while 42:
+        clientsocket = socket(AF_INET, SOCK_STREAM)
+        clientsocket.bind(('localhost', 53000))
+        clientsocket.listen(1)
+        resp, address = clientsocket.accept()
+        resp = resp.recv(1024)
+        print(resp.decode())
+        clientsocket.close()
+        break
+tempo_final = time.perf_counter()
+print(f'tempo para resoluir as 5 requisições: {tempo_final - tempo_inicial}')
