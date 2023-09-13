d2e6c9e1dbcc9c429547518912cd1c08e25393e6
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-13 02:00:02 -0300

    Update client_udp.py

--- client_udp.py
+++ client_udp.py
@@ -3,6 +3,8 @@ from socket import *
 name_server = "localhost"
 dns_port = 51009
 
+anos  = ["06/10", "01/09", "19/03", "22/01", "29/12"]
+
 def request_dns(name_server, dns_port):
     msg = f"request,{name_server}, UDP"
     sock_client = socket(AF_INET, SOCK_DGRAM)
@@ -19,5 +21,8 @@ print("Server On")
 client = socket(AF_INET, SOCK_DGRAM)
 messagem = "informe o ano de nascimento"
 
-client.sendto(messagem.encode(), (name_server, int(server_port)))
+client.bind(("localhost", 52000))
+client.sendto(anos[0].encode(), (name_server, int(server_port)))
+resp, ip = client.recvfrom(1024)
+print(resp.decode())
 client.close()
