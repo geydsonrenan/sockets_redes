88fd0c675854b3a93678410a891137f5ce4c9418
geydsonrenan <114956476+geydsonrenan@users.noreply.github.com>
2023-09-14 17:58:35 -0300

    Update dns.py

--- dns.py
+++ dns.py
@@ -8,25 +8,33 @@ port = 51009
 server = socket(AF_INET, SOCK_DGRAM)
 print("Starting Server...")
 server.bind((name, port))
-print("Server ON!")
+print("Server DNS ON!")
 
 while 42:
     msg, ip = server.recvfrom(1024)
     msg = msg.decode()
     msg = msg.split(',')
+    print(msg)
     
     if msg[0] == "gravar":
-        dns[msg[1]+msg[-1]] = msg[2]
+        dns[msg[-2]+msg[-1]] = msg[1]+' '+msg[2]
         print(f"registrou o {msg[1]} no DNS.")
+        server.sendto(f'{msg[-2]+msg[-1]} registrado com sucesso'.encode(), ip)
     
     elif msg[0] == "request":
-        if msg[1]+msg[-1] in dns:
+        try:
+            print('.')
             server.sendto(dns[msg[1]+msg[-1]].encode(), ip)
-        else:
+        except:
             print("DNS não encontrado.")
+            server.sendto('Not Found'.encode(), ip)
             
-    elif msg[0] == 'deletar':
-        dns.pop(msg[1])
-        print(f"Endereço {msg[2]} deletado do servidor.")
-    print(dns)
+    elif msg[0] == 'delete':
+        del dns[msg[1]+msg[-1]]
+        print(f"Endereço {msg[1]+msg[-1]} deletado do servidor.")
+        server.sendto(f'Endereço {msg[1]} deletado com sucesso'.encode(), ip)
+        #if dns == {}:
+            #break
+server.close()
+
 
